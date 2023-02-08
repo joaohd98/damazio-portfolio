2dda66d8d848553646d59d51ec97487f1d411c20
João Damazio <joao.damazio@fiap.com.br>
2023-02-08 01:55:06 -0300

    fix(fonts): change uppercase for roboto

--- src/styles/fonts/roboto.ts
+++ src/styles/fonts/roboto.ts
@@ -4,21 +4,21 @@ const GlobalRoboto = createGlobalStyle`
 
   @font-face {
     font-family: 'Roboto';
-    src: url('./fonts/roboto/roboto-black.ttf') format('truetype');
+    src: url('./fonts/roboto/Roboto-black.ttf') format('truetype');
     font-weight: 800;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Roboto';
-    src: url('./fonts/roboto/roboto-bold.ttf') format('truetype');
+    src: url('./fonts/roboto/Roboto-bold.ttf') format('truetype');
     font-weight: 700;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Roboto';
-    src: url('./fonts/roboto/roboto-medium.ttf') format('truetype');
+    src: url('./fonts/roboto/Roboto-medium.ttf') format('truetype');
     font-weight: 500;
     font-style: normal;
   }
@@ -26,7 +26,7 @@ const GlobalRoboto = createGlobalStyle`
 
   @font-face {
     font-family: 'Roboto';
-    src: url('./fonts/roboto/roboto-light.ttf') format('truetype');
+    src: url('./fonts/roboto/Roboto-light.ttf') format('truetype');
     font-weight: 300;
     font-style: normal;
   }
@@ -34,7 +34,7 @@ const GlobalRoboto = createGlobalStyle`
 
   @font-face {
     font-family: 'Roboto';
-    src: url('./fonts/roboto/roboto-thin.ttf') format('truetype');
+    src: url('./fonts/roboto/Roboto-thin.ttf') format('truetype');
     font-weight: 200;
     font-style: normal;
   }
